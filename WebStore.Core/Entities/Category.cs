5d6d91235be2ad0c2c4b705a2458b94d140fc6e6
Jahanvi Sharma <56484763+jahanvisharma-dotcom@users.noreply.github.com>
2022-08-06 04:14:34 +0530

    Add files via upload

---
+++ WebStore.Core/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace WebStore.Core.Entities
+{
+    public class Category
+    {
+        public Category()
+        {
+            Products = new List<Product>();
+        }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
